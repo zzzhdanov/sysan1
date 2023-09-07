a1e7792614a63398499c8e801c8c532a30658cc3
zzzhdanov <100018094+zzzhdanov@users.noreply.github.com>
2023-09-07 12:01:28 +0300

    Update main.py

--- task1/main.py
+++ task1/main.py
@@ -2,16 +2,14 @@ import csv
 
 
 def get_item(filename, string_number, column_number):
-    num = 0
-    
+
     if string_number < 0 or column_number < 0:
         raise ValueError("OUT OF RANGE")
 
     with open(filename, 'r', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in reader:
-            num += 1
-            if num == string_number + 1:
+        for index, row in enumerate(reader):
+            if index == string_number:
                 return row[column_number - 1]
 
 
